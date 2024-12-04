034dc426eae9cc112b20fffeb73cabab64cc1b6b
Tola-lemma <tolalemma@gmail.com>
2024-12-04 20:57:53 +0300

    feat:root style is imported

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import {App} from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './API/store';
+import './index.css'
 import { BrowserRouter as Router } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
