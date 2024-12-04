6a5e6744df863bdb522c814875fc1a5bb12683f9
Tola-lemma <tolalemma@gmail.com>
2024-12-04 14:32:14 +0300

    feat:constant like project name and colors are added

---
+++ src/Constants/constant.jsx
@@ -0,0 +1,17 @@
+export const NAME = "E-COMMERCE WEB APP"
+export const colors = {
+      background: {
+        primary:  "#882BEC",
+        secondary:  "#FEA301",
+        tertiary: "#61CE70",
+        outline: "#C1C1C1",
+        gray:'#717D8A',
+        lightGray:'#5D6A78',
+        warning:  "#0071BC",
+        error: "#EE5D5D",
+        green:  "#8EDF87",
+        black:'#000000',
+        darkLayerAppBar: "#EAEAEA",
+        pureWhite: "#ffffff",
+      },
+    };
