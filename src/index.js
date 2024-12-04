604293ab43828fdf1d3644d78c4f2fb7daf1bc16
Tola-lemma <tolalemma@gmail.com>
2024-12-04 14:33:17 +0300

    feat:browser router is added to  handle page routing

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
-import App from './App';
+import {App} from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './API/store';
-
+import { BrowserRouter as Router } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
      <Provider store={store}>
-       <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>
 );
