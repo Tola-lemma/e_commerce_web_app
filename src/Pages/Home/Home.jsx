417cfdf0ee44e711330ba96d2d5661fc0ebf9b42
Tola-lemma <tolalemma@gmail.com>
2024-12-04 23:22:59 +0300

    feat:header and footer components removed because of Layout created

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,17 +1,11 @@
 import React from 'react'
 import { Box } from '@mui/material'
-import { TopHeader } from '../Header/TopHeader'
-import { MainHeader } from '../Header/MainHeader'
-
 const Home = () => {
   return (
    <Box sx={{
-      // margin: 0,
-      // padding: 0,
       width: "100%",
     }}>
-   <TopHeader/>
-   <MainHeader/>
+   <h1>Welcome to E-Commerce Web App</h1>
    </Box>
   )
 }
