0e055710f9e23d7d4f2dacae4a473ef44bc8805b
Tola-lemma <tolalemma@gmail.com>
2024-12-04 23:21:19 +0300

    feat:Layout to handle header and footer is implemented

---
+++ src/Pages/Layout.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Box } from '@mui/material';
+import { TopHeader } from '../Pages/Header/TopHeader';
+import { MainHeader } from '../Pages/Header/MainHeader';
+import { Footer } from '../Pages/Footer/Footer';
+
+const Layout = ({ children }) => {
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh'}}>
+      {/* Fixed Header */}
+      <Box sx={{ position: 'fixed', top: 0, left: 0, width: '100%', zIndex: 1000 }}>
+        <TopHeader />
+        <MainHeader />
+      </Box>
+
+      {/* Main Content */}
+      <Box sx={{ flexGrow: 1, mt: '96px', p: 2 }}>
+        {children}
+      </Box>
+
+      {/* Footer */}
+      <Footer />
+    </Box>
+  );
+};
+
+export default Layout;
