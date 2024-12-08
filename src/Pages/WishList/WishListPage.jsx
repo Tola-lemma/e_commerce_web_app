c3dc4c805787f8518e22c4f7ead52017dd6f1402
Tola-lemma <tolalemma@gmail.com>
2024-12-08 18:56:54 +0300

    feat:wishlist product recommndation

--- src/Pages/WishList/WishListPage.jsx
+++ src/Pages/WishList/WishListPage.jsx
@@ -2,9 +2,24 @@ import { Box, Typography } from "@mui/material";
 import { useWishlist } from "../WishList/WishListContext";
 import ImageCards from "../ReUsableComponents/ReusableCard";
 import CustomBreadcrumbs from "../ReUsableComponents/BreadCrumb";
+import { useGetProductByCategoryQuery } from "../../Features/productApiSlice";
+import LoadingSkeleton from "../ReUsableComponents/FixedTextSkeleton";
 
 const WishlistPage = () => {
   const { wishlist } = useWishlist();
+  const {data:getProductByCategory,isLoading:categoryLoading,error:getCategoryError} = useGetProductByCategoryQuery(
+      { category: 'electronics' }
+    );
+  const categoryData=getProductByCategory?.data.map((item) => ({
+    id:item.id,
+    imageSrc: item.image, 
+    description:item.description,
+    title: item.title, 
+    category: item.category,
+    price: item.price,
+    rate: item.rate,
+    ratingCount: item.ratingCount 
+  })) || [];
   const breadcrumbsItems = [
       { name: "WishList-page" }
   ];
@@ -52,7 +67,29 @@ const WishlistPage = () => {
       )}
       <ImageCards  cardData={wishlist} /> 
     </Box>
-    
+      {categoryLoading? <LoadingSkeleton/>:(<Box> 
+      <Typography
+            variant="body1"
+            color="initial"
+            sx={{
+              color: "#DB4444",
+              fontFamily: "Poppins",
+              fontSize: "18px",
+              borderLeft: "10px solid #DB4444",
+              padding: 1,
+              marginLeft: "6px",
+            }}
+          >
+           Just For You
+          </Typography>
+          </Box>)}
+          <ImageCards  cardData={categoryData} /> 
+          {(getCategoryError ) && (
+          <h1 style={{ color: "red", fontSize: "24px", textAlign: "center" }}>
+            An unexpected error occurred:{" "}
+            {getCategoryError.error || "Unknown Error"}
+          </h1>
+        )}
     </>
   );
 };
